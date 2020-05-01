0f7642de8e6434a54f1c1fb8e9b4bd8761510428
James <jmcelroy@plexustech.net>
2020-05-01 13:02:44 -0500

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,6 +3,20 @@
 # By convention, the class name should match the part of the
 # file name after the number, so in this case:
 # 002_create_costumes.rb becomes class CreateCostumes
+class CreateCostumes < ActiveRecord::Migration[5.2]
+    def change
+        create_table :costumes do |t|
+            t.string :name
+            t.string :size
+            t.string :image_url
+            t.float :price
+
+            t.timestamps null: false
+        end
+    end
+
+
+ end
 
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
